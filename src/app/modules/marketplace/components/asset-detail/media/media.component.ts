3eb53bccbf25746edef5024a4b2d315d0c8e7bd4
Héctor Bonell Pradas <hbonell@iti.es>
2024-06-25 13:28:01 +0200

    fix asset detail media card properties show

--- src/app/modules/marketplace/components/asset-detail/media/media.component.ts
+++ src/app/modules/marketplace/components/asset-detail/media/media.component.ts
@@ -1,14 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Media } from '@app/shared/models/media.model';
+import { Component, Input, OnInit } from "@angular/core";
+import { Media } from "@app/shared/models/media.model";
 
 @Component({
-  selector: 'app-media',
-  templateUrl: './media.component.html',
-  styleUrls: ['./media.component.scss']
+  selector: "app-media",
+  templateUrl: "./media.component.html",
+  styleUrls: ["./media.component.scss"],
 })
 export class MediaComponent {
   @Input() public media!: Media;
   public panelOpenState = false;
 
   constructor() {}
+
+  existsMediaProps() {
+    if (
+      this.media.checksumAlgorithm ||
+      this.media.encodingFormat ||
+      this.media.contentSizeKb ||
+      this.media.checksum ||
+      this.media.copyright ||
+      this.media.datePublished
+    ) {
+      return true;
+    }
+
+    return false;
+  }
 }
