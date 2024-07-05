9ff953ed7c22f3968ef6ee66d714c55173d5a610
Antoni Gimeno <agimenobono@iti.es>
2024-07-05 13:21:09 +0200

    fix: entrypoint script to inject env variables

--- docker/marketplace/script.sh
+++ docker/marketplace/script.sh
@@ -7,4 +7,7 @@ export MY_PATH_API_MY_LIB
 envsubst '${MY_PATH_API_MY_LIB},${MY_PATH_API_AIOD_CATALOGUE}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 envsubst < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
 
-nginx -g 'daemon off;'
+# nginx -g 'daemon off;'
+
+# Execute the CMD passed to the container
+exec "$@"
