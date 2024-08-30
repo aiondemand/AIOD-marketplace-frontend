33ebc4f41ad39966483548d718006b265a457f60
Ubuntu <agimenobono@iti.es>
2024-08-30 11:14:27 +0000

    fix: solved issues on env vars injection when start container

--- docker/marketplace/script.sh
+++ docker/marketplace/script.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-export MY_PATH_API_AIOD_CATALOGUE
-export MY_PATH_API_MY_LIB
-
-
+# Substitute environment variables in env.template.js and output the result to env.js
 envsubst < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
-
-# nginx -g 'daemon off;'
 
 # Execute the CMD passed to the container
 exec "$@"
