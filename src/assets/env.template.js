9a4d60d185e7ec7afb2ba6cb4496ef5ccdff3612
hector <andresvicentehector@gmail.com>
2025-06-09 15:18:03 +0200

    feat(env): zoho script only accessible on production

--- src/assets/env.template.js
+++ src/assets/env.template.js
@@ -6,4 +6,5 @@
   window['env']['API_URL'] = '${API_URL}';
   window['env']['AIOD_ENHANCED_API'] = '${AIOD_ENHANCED_API}';
   window['env']['ML_REDIRECT_URI'] = '${ML_REDIRECT_URI}'
+  window['env']['ZOHO_URL']='${ZOHO_URL}';
 })(this);
