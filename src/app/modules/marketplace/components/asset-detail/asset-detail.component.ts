47047120a5647ffdb14fb79d08b21a1aa522346b
PGijsbers <p.gijsbers@tue.nl>
2025-06-22 12:08:01 +0200

    Update default identifiers to match new type

--- src/app/modules/marketplace/components/asset-detail/asset-detail.component.ts
+++ src/app/modules/marketplace/components/asset-detail/asset-detail.component.ts
@@ -55,12 +55,12 @@ export class AssetDetailComponent implements OnInit, OnDestroy{
         setTimeout(() => (this.isLoading = false), 3000)
         console.error('Error get asset', error)
         this.asset={
-          "identifier": 24,
+          "identifier": "data_02E1OuNCoTvFQPkYTkHDCOLX",
           "category": AssetCategory.Dataset,
           "name": "bigIR/ar_cov19",
           "description": "ArCOV-19 is an Arabic COVID-19 Twitter dataset that covers the period from 27th of January till 30th of April 2020. ArCOV-19 is designed to enable research under several domains including natural language processing, information retrieval, and social computing, among others",
           "platform": "huggingface",
-          "platform_resource_identifier":11111111,
+          "platform_resource_identifier": "11111111",
           "keywords": [
               "source_datasets:original",
               "multilinguality:monolingual",
