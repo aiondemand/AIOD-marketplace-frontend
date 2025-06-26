b4a6087f99c519d5d7c35e43ad6e5d77d40fd57b
Andrea <bal.andrea.ogh@gmail.com>
2025-06-26 15:58:46 +0100

    update model, remove unused properties

--- src/app/shared/models/caseStudy.model.ts
+++ src/app/shared/models/caseStudy.model.ts
@@ -4,28 +4,17 @@ import { AssetModel } from "./asset.model";
 
 export class CaseStudyModel extends AssetModel {
   aiodEntry: AiodEntry;
-  timeRequired: string;
-  accessMode: string[];
   aiResourceIdentifier: number;
   applicationArea: string[];
   contact: any[];
-  content: string;
   creator: string[];
-  educationalLevel: string[];
   hasPart: any[];
-  inLanguage: string[];
   industrialSector: string[];
   isPartOf: any[];
-  location: any[];
   note: any[];
-  pace: string;
-  prerequisite: string[];
   relevantLink: string[];
   relevantResource: any[];
   relevantTo: any[];
-  targetAudience: string[];
-  type: string;
-
   isAccessibleForFree: string;
   aiAssetIdentifier: string;
 
@@ -53,17 +42,5 @@ export class CaseStudyModel extends AssetModel {
     this.relevantLink = data.relevant_link;
     this.relevantResource = data.relevant_resource;
     this.relevantTo = data.relevant_to;
-
-
-    this.timeRequired = data.time_required;
-    this.accessMode = data.access_mode;
-    this.content = data.content?.plain;
-    this.educationalLevel = data.educational_level;
-    this.inLanguage = data.in_language;
-    this.location = data.location;
-    this.pace = data.pace;
-    this.prerequisite = data.prerequisite;
-    this.targetAudience = data.target_audience;
-    this.type = data.type;
   }
 }
