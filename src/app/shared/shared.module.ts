0ad417e88444e3579f1fc5cc554c6bafb5c40eff
jtortola <jtortola@iti.es>
2025-07-25 10:18:49 +0200

    chore: merge main staging

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { MaterialModule } from './material.module';
@@ -14,6 +14,7 @@ import { BreadcrumbModule } from 'xng-breadcrumb';
   imports: [CommonModule, MaterialModule],
   exports: [
     ReactiveFormsModule,
+    FormsModule,
     MaterialModule,
     RouterModule,
     TranslateModule,
