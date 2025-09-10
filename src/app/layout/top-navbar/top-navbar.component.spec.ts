ddc00f72ca327da557525617abeccecbe3b402d0
hector <andresvicentehector@gmail.com>
2025-09-10 09:23:55 +0200

    fix: adding some moke of matchMedia query in top-navbar component test

--- src/app/layout/top-navbar/top-navbar.component.spec.ts
+++ src/app/layout/top-navbar/top-navbar.component.spec.ts
@@ -25,6 +25,22 @@ const mockedMediaQueryList: MediaQueryList = {
 const mockedMediaMatcher: any = {
   matchMedia: jest.fn().mockReturnValue(mockedMediaQueryList),
 };
+// Mock window.matchMedia
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: (query: string) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    }),
+  });
+});
 
 describe('TopNavbarComponent', () => {
   let component: TopNavbarComponent;
