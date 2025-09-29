ceccaca05a8355bbc2159af5d87221635b23464b
jtortola <jtortola@iti.es>
2025-09-29 10:02:48 +0200

    chore: extract links to common file

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { EXTERNAL_LINKS } from '@app/shared/constants/external-links';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  protected externalLinks = EXTERNAL_LINKS;
+}
